ef1f115f89e200b7c9cab6889168eac7be414a0b
ILLPending <131606739+ILLPending@users.noreply.github.com>
2023-08-04 10:40:24 +0100

    Update level-service.service.ts
    undone test code

--- src/app/shared/level-service.service.ts
+++ src/app/shared/level-service.service.ts
@@ -8,45 +8,43 @@ import Pocketbase from 'pocketbase'
 export class LevelServiceService {
   constructor() {
   }
-  if (false){
-    pb = new Pocketbase('https://pb.impossible-list.com')
-  
-  
-    getEntireLevelList() {
-      return this.pb.collection('ill').getFullList<ImpossibleLevel>(200)
+  pb = new Pocketbase('https://pb.impossible-list.com')
+
+
+  getEntireLevelList() {
+    return this.pb.collection('ill').getFullList<ImpossibleLevel>(200)
+  }
+
+  getWholeLevelList() {
+    return this.pb.collection('ill').getFullList<ImpossibleLevel>(200)
+  }
+
+  async getOrderedLevelList() {
+    let arr = await this.pb.collection('ill').getFullList<ImpossibleLevel>(100, { sort: 'position', $autoCancel: false})
+    return arr
+  }
+
+  addLevel(level:ImpossibleLevel) {
+    
+    // level.id = record.id
+    return this.pb.collection("ill").create(level);
+  }
+
+  updateLevel(level:ImpossibleLevel) {
+    if(level.id) {
+      return this.pb.collection("ill").update(level.id, level);
+    } else {
+      console.error('invalid id lol')
+      return undefined
     }
-  
-    getWholeLevelList() {
-      return this.pb.collection('ill').getFullList<ImpossibleLevel>(200)
-    }
-  
-    async getOrderedLevelList() {
-      let arr = await this.pb.collection('ill').getFullList<ImpossibleLevel>(100, { sort: 'position', $autoCancel: false})
-      return arr
-    }
-  
-    addLevel(level:ImpossibleLevel) {
-      
-      // level.id = record.id
-      return this.pb.collection("ill").create(level);
-    }
-  
-    updateLevel(level:ImpossibleLevel) {
-      if(level.id) {
-        return this.pb.collection("ill").update(level.id, level);
-      } else {
-        console.error('invalid id lol')
-        return undefined
-      }
-    }
-  
-    deleteLevel(level:ImpossibleLevel) {
-      if(level.id) {
-        return this.pb.collection(`ill`).delete(level.id)
-      } else {
-        console.error('invalid id lol')
-        return undefined
-      }
+  }
+
+  deleteLevel(level:ImpossibleLevel) {
+    if(level.id) {
+      return this.pb.collection(`ill`).delete(level.id)
+    } else {
+      console.error('invalid id lol')
+      return undefined
     }
   }
 }
